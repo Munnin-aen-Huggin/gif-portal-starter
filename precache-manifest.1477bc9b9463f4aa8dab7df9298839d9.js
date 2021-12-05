1d8d013272f03ed00917faf6d298bfbfe1952410
Munnin-aen-Huggin <kron@fajksv.redcrp>
2021-12-06 01:12:11 +0300

    Updates

---
+++ precache-manifest.1477bc9b9463f4aa8dab7df9298839d9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9fafd900d8b3390e182a6d30be55ddd5",
+    "url": "/%7BCFC-SOL-Dapp%7D/index.html"
+  },
+  {
+    "revision": "5a5a20dcff204baf0f00",
+    "url": "/%7BCFC-SOL-Dapp%7D/static/css/main.5f01ab23.chunk.css"
+  },
+  {
+    "revision": "8a68d59f32203877bb5b",
+    "url": "/%7BCFC-SOL-Dapp%7D/static/js/2.0bcf4adb.chunk.js"
+  },
+  {
+    "revision": "ae3b41f871e37084bf694df00965c2cf",
+    "url": "/%7BCFC-SOL-Dapp%7D/static/js/2.0bcf4adb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5a5a20dcff204baf0f00",
+    "url": "/%7BCFC-SOL-Dapp%7D/static/js/main.eade624c.chunk.js"
+  },
+  {
+    "revision": "9b522b2cf6471e52a32a",
+    "url": "/%7BCFC-SOL-Dapp%7D/static/js/runtime-main.cf7123fd.js"
+  },
+  {
+    "revision": "5c97ee2a3ed91e9de148f8d40b2d324e",
+    "url": "/%7BCFC-SOL-Dapp%7D/static/media/twitter-logo.5c97ee2a.svg"
+  }
+]);
